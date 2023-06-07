0488db2f7014051e064bc00312de2bdd60b95b7c
SynchDescronDev <SynchDescronDev@gmail.com>
2023-06-07 11:49:42 +0300

    Create Main.java

---
+++ Strings/Main.java
@@ -0,0 +1,15 @@
+package Strings;
+
+public class Main {
+    public static void main(String[] args) {
+        String name = "Jack Sparrow";
+        System.out.println(name.concat(" Captain"));
+        System.out.println(name.substring(5));
+        System.out.println(name.length());
+        System.out.println(name.charAt(0));
+        System.out.println(name.equals("Jack"));
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+        System.out.println(name.contains("Jack"));
+    }
+}
