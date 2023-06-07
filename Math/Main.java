f3ace8f90b80b0f7b85726417183bc1ef479b178
SynchDescronDev <SynchDescronDev@gmail.com>
2023-06-07 11:52:58 +0300

    Create Main.java

---
+++ Math/Main.java
@@ -0,0 +1,20 @@
+package Math;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 2;
+        int z = -10;
+        double i = 3.14;
+
+        System.out.println(Math.abs(z));
+        System.out.println(Math.max(x, y));
+        System.out.println(Math.min(x, y));
+        System.out.println(Math.floor(i));
+        System.out.println(Math.ceil(i));
+        System.out.println(Math.pow(x, y));
+        System.out.println(Math.sqrt(x));
+        System.out.println(Math.round(i));
+        
+    }
+}
