cd22fca0c543d8ed21b0e681daa49192a549693e
SynchDescronDev <SynchDescronDev@gmail.com>
2023-06-07 12:39:18 +0300

    Create Main.java

---
+++ Loop/Main.java
@@ -0,0 +1,18 @@
+package Loop;
+
+public class Main {
+    public static void main(String[] args) {
+        for(int i=1;i<4;i++){
+            System.out.println("i :"+i);
+        }
+        int j = 1;
+        while(j<4){
+            System.out.println("j :"+j);
+            j++;
+        }
+        int z = 5;
+        do{
+            System.out.println("z :"+z);
+        }while(z<4);
+    }
+}
