8f12e55d52002e27906d4a58a1f520c573adb7f7
SynchDescronDev <SynchDescronDev@gmail.com>
2023-06-07 10:17:48 +0300

    Create Main.java

---
+++ Variables/Main.java
@@ -0,0 +1,27 @@
+package Variables;
+
+public class Main {
+    public static void main(String[] args) {
+        byte byteNumber = 127;
+        short shortNumber = 32000;
+        int intNumber = -15;
+        long longNumber = -2000;
+        float floatNumber = 3.14F;
+        double doubleNumber = 2222.2222;
+        char character = 'T';
+        boolean myBoolean = true;
+        String name = "Jack Sparrow";
+
+
+        System.out.println(byteNumber);
+        System.out.println(shortNumber);
+        System.out.println(intNumber);
+        System.out.println(longNumber);
+        System.out.println(floatNumber);
+        System.out.println(doubleNumber);
+        System.out.println(character);
+        System.out.println(myBoolean);
+        System.out.println(name);
+
+    }
+}
