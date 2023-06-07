7888af4ef7b4c53ab7a3a8395d5dabbd23189e40
SynchDescronDev <SynchDescronDev@gmail.com>
2023-06-07 11:41:54 +0300

    Create Main.java

---
+++ TypeCasting/Main.java
@@ -0,0 +1,18 @@
+package TypeCasting;
+
+public class Main {
+    public static void main(String[] args) {
+        int num = 5;
+        double doubleNum = num;
+        System.out.println(num);
+        System.out.println(doubleNum);
+
+        double number = 10.0;
+        int intNum = (int) number;
+        System.out.println(number);
+        System.out.println(intNum);
+
+        
+        
+    }
+}
